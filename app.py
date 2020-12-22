05ecf81a8985ac493b585ce4c59c30508de27dbc
shungame65 <51528839+shungame65@users.noreply.github.com>
2020-12-22 21:51:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -79,76 +79,79 @@ def home():
     if "email" in session: 
         # if yes, was the form sent? 
         if request.method == "POST": 
-            #getting information from the form
-            rule = request.form["rule"]
-            rank1 = int(request.form["rank1"])
-            rank2 = int(request.form["rank2"])
-            rank3 = int(request.form["rank3"])
-            rank4 = int(request.form["rank4"])
-            ranks = [rank1, rank2, rank3, rank4]
-            Averagerank= statistics.mean(ranks)
-            nog = request.form["nog"]
-            stage = request.form["stage"]
-            friendcode = request.form["friendcode"]
-            note = request.form["note"]
-            # Any row with the same email? 
-            foundmatch = matchestable.query.filter_by(email=session["email"]).all()
-            foundpair1 = pairs.query.filter_by(email1=session["email"]).all()
-            foundpair2 = pairs.query.filter_by(email2=session["email"]).all()
-            #if yes, delete these rows
-            if foundmatch or foundpair1 or foundpair2: 
-                for match in foundmatch: 
+            if request.form["friendcode"] == '' or request.form["note"] == '':
+                 return render_template("main.html", emptyfields="＊＊＊フレンドコード、または備考を埋めてから募集してください＊＊＊", similarmatch=None)
+            else:
+                #getting information from the form
+                rule = request.form["rule"]
+                rank1 = int(request.form["rank1"])
+                rank2 = int(request.form["rank2"])
+                rank3 = int(request.form["rank3"])
+                rank4 = int(request.form["rank4"])
+                ranks = [rank1, rank2, rank3, rank4]
+                Averagerank= statistics.mean(ranks)
+                nog = request.form["nog"]
+                stage = request.form["stage"]
+                friendcode = request.form["friendcode"]
+                note = request.form["note"]
+                # Any row with the same email? 
+                foundmatch = matchestable.query.filter_by(email=session["email"]).all()
+                foundpair1 = pairs.query.filter_by(email1=session["email"]).all()
+                foundpair2 = pairs.query.filter_by(email2=session["email"]).all()
+                #if yes, delete these rows
+                if foundmatch or foundpair1 or foundpair2: 
+                    for match in foundmatch: 
+                        db.session.delete(match)
+                    for pair in foundpair1: 
+                        db.session.delete(pair)
+                    for pair in foundpair2: 
+                        db.session.delete(pair)
+                    db.session.commit()
+                #comparing his rank to other people's ranks on matches table 
+                similarmatch = matchestable.query.filter(and_(matchestable.averagerank >= Averagerank-1, matchestable.averagerank <= Averagerank+1, matchestable.rule == rule)).first()
+                if similarmatch: 
+                    #adding to pairs table 
+                    pair = pairs(email1=session["email"], email2=similarmatch.email)
+                    db.session.add(pair)
+                    #adding to pending table 
+                    pendingmatch = pendingmatches(email=session["email"] ,rule=rule, averagerank=Averagerank, nog=nog, stage=stage, friendcode=friendcode, note=note)
+                    db.session.add(pendingmatch)
+                    #deleting similar matches 
+                    db.session.delete(similarmatch)
+                    db.session.commit()
+                    #getting data
+                    dbmatch = matchestable.query.all()
+                    dbusers = users.query.all()
+                    dbpairs = pairs.query.all()
+                    return render_template("main.html", matches=dbmatch, users=dbusers, pairs=dbpairs, similarmatch=similarmatch, message="マッチングしました。部屋立てとフレンド申請はあなたが行ってください↓↓↓↓↓↓↓↓↓")
+                else: 
+                    #no player with similar ranks
+                    match = matchestable(email=session["email"] ,rule=rule, averagerank=Averagerank, nog=nog, stage=stage, friendcode=friendcode, note=note)
+                    db.session.add(match)
+                    # foundpair = pairs.query.all()
+                    # for pair in foundpair: 
+                    #     db.session.delete(pair)
+                    db.session.commit()
+                
+                    count = 0
+                    while count < 60: 
+                        time.sleep(5)
+                        inpairstable = pairs.query.filter_by(email2=session["email"]).first()
+                        if inpairstable: 
+                            opponentsemail = inpairstable.email1
+                            similarmatch = pendingmatches.query.filter_by(email=opponentsemail).first()
+                            db.session.delete(similarmatch)
+                            db.session.commit()
+                            dbmatch = matchestable.query.all()
+                            dbusers = users.query.all()
+                            dbpairs = pairs.query.all()
+                            return render_template("main.html", matches=dbmatch, users=dbusers, pairs=dbpairs, similarmatch=similarmatch, message="マッチングしました。部屋たてとフレンド申請は相手が行います↓↓↓↓↓↓↓↓↓")
+                        else:
+                            count += 10
                     db.session.delete(match)
-                for pair in foundpair1: 
-                    db.session.delete(pair)
-                for pair in foundpair2: 
-                    db.session.delete(pair)
-                db.session.commit()
-            #comparing his rank to other people's ranks on matches table 
-            similarmatch = matchestable.query.filter(and_(matchestable.averagerank >= Averagerank-1, matchestable.averagerank <= Averagerank+1, matchestable.rule == rule)).first()
-            if similarmatch: 
-                #adding to pairs table 
-                pair = pairs(email1=session["email"], email2=similarmatch.email)
-                db.session.add(pair)
-                #adding to pending table 
-                pendingmatch = pendingmatches(email=session["email"] ,rule=rule, averagerank=Averagerank, nog=nog, stage=stage, friendcode=friendcode, note=note)
-                db.session.add(pendingmatch)
-                #deleting similar matches 
-                db.session.delete(similarmatch)
-                db.session.commit()
-                #getting data
-                dbmatch = matchestable.query.all()
-                dbusers = users.query.all()
-                dbpairs = pairs.query.all()
-                return render_template("main.html", matches=dbmatch, users=dbusers, pairs=dbpairs, similarmatch=similarmatch, message="マッチングしました。部屋立てとフレンド申請はあなたが行ってください↓↓↓↓↓↓↓↓↓")
-            else: 
-                #no player with similar ranks
-                match = matchestable(email=session["email"] ,rule=rule, averagerank=Averagerank, nog=nog, stage=stage, friendcode=friendcode, note=note)
-                db.session.add(match)
-                # foundpair = pairs.query.all()
-                # for pair in foundpair: 
-                #     db.session.delete(pair)
-                db.session.commit()
-            
-                count = 0
-                while count < 20: 
-                    time.sleep(5)
-                    inpairstable = pairs.query.filter_by(email2=session["email"]).first()
-                    if inpairstable: 
-                        opponentsemail = inpairstable.email1
-                        similarmatch = pendingmatches.query.filter_by(email=opponentsemail).first()
-                        db.session.delete(similarmatch)
-                        db.session.commit()
-                        dbmatch = matchestable.query.all()
-                        dbusers = users.query.all()
-                        dbpairs = pairs.query.all()
-                        return render_template("main.html", matches=dbmatch, users=dbusers, pairs=dbpairs, similarmatch=similarmatch, message="マッチングしました。部屋たてとフレンド申請は相手が行います↓↓↓↓↓↓↓↓↓")
-                    else:
-                        count += 10
-                db.session.delete(match)
-                db.session.commit()
+                    db.session.commit()
 
-                return render_template("main.html", message="見つかりませんでした", similarmatch=None)
+                    return render_template("main.html", message="見つかりませんでした", similarmatch=None)
         #if no, just render template
         else: 
             return render_template("main.html", email=session["email"], similarmatch=None)
@@ -159,6 +162,11 @@ def home():
 @app.route("/aboutthiswebsite")
 def aboutthiswebsite():
     return render_template("aboutthiswebsite.html")
+
+@app.route("/howtouse")
+def howtouse():
+    return render_template("使い方.html")
+
 
 if __name__ == "__main__":
     db.create_all()
